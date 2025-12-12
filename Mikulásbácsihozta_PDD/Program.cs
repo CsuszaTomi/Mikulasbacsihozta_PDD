a3f671cb68c7ffa03e2119df40ad3401694daa68
Dunka <pappd1@kkszki.hu>
2025-12-12 10:28:45 +0100

    Versenyző hozzáadás fix

--- Mikulásbácsihozta_PDD/Program.cs
+++ Mikulásbácsihozta_PDD/Program.cs
@@ -13,13 +13,13 @@ namespace Mikulásbácsihozta_PDD
     {
         static void Main(string[] args)
         {
+            MySqlConnection connection = new MySqlConnection();
+            string connectionString = "SERVER = localhost,DATABASE=kalaplengetőverseny_pdd;UID=root;PASSWORD=;";
+            connection.ConnectionString = connectionString;
+            List<User> users = new List<User>();
             while (true)
             {
                 Console.Clear();
-                MySqlConnection connection = new MySqlConnection();
-                string connectionString = "SERVER = localhost,DATABASE=kalaplengetőverseny_pdd;UID=root;PASSWORD=;";
-                connection.ConnectionString = connectionString;
-                List<User> users = new List<User>();
                 users = UserController.GetUsers();
                 MainMenu(connection,users);
             }
@@ -31,6 +31,7 @@ namespace Mikulásbácsihozta_PDD
             switch (menu)
             {
                 case 0:
+                    users = UserController.GetUsers();
                     foreach (var user in users)
                     {
                         TextDecoration.WriteLineCentered($"ID: {user.Id} Név: {user.Nev} Pont1: {user.pont1} Idő1: {user.ido1} Pont2: {user.pont2} Idő2: {user.ido2} Pont3: {user.pont3} Idő3: {user.ido3} LegjobbPont: {user.Legjobbpont} LegjobbIdő: {user.Legjobbido}");
@@ -40,29 +41,48 @@ namespace Mikulásbácsihozta_PDD
                     break;
                 case 1:
                     #region Felhasználó felvétele
-                    Console.WriteLine("=== ÚJ FELHASZNÁLÓ RÖGZÍTÉSE ===");
-                    Console.WriteLine("Add meg a felhasználó nevét: ");
+                    TextDecoration.WriteLineCentered("=== ÚJ FELHASZNÁLÓ RÖGZÍTÉSE ===");
+                    TextDecoration.WriteCentered("Add meg a versenyző nevét: ");
                     string nev = Console.ReadLine();
-                    Console.WriteLine("Add meg a felhasználó login nevét: ");
-                    string loginName = Console.ReadLine();
-                    Console.WriteLine("A felhasználó aktív-e? (i/n): ");
-                    string aktivInput = Console.ReadLine();
-                    bool aktiv = aktivInput.ToLower() == "i" ? true : false;
-                    Console.WriteLine("Add meg a saltot: ");
-                    string salt = Console.ReadLine();
-                    Console.WriteLine("Add meg a hasht: ");
-                    string hash = Console.ReadLine();
-                    Console.WriteLine("Add meg a felhasználó születési dátumát (éééé-hh-nn): ");
-                    DateTime szuldat = DateTime.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző első körben elért pontszámát: ");
+                    int pont1 = int.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző első körben elért idejét: ");
+                    double ido1 = double.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző második körben elért pontszámát: ");
+                    int pont2 = int.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző második körben elért idejét: ");
+                    double ido2 = double.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző harmadik körben elért pontszámát: ");
+                    int pont3 = int.Parse(Console.ReadLine());
+                    TextDecoration.WriteCentered("Add meg versenyző harmadik körben elért idejét: ");
+                    double ido3 = double.Parse(Console.ReadLine());
+                    int legjobbpont = Math.Max(pont1, Math.Max(pont2, pont3));
+                    double legjobbido = Math.Min(ido1, Math.Min(ido2, ido3));
+                    int pillanatnyihelyezes = 1;
+                    foreach (var user in users)
+                    {
+                        if (legjobbpont < user.Legjobbpont)
+                        {
+                            pillanatnyihelyezes++;
+                        }
+                        else if (legjobbpont == user.Legjobbpont && legjobbido > user.Legjobbido)
+                        {
+                            pillanatnyihelyezes++;
+                        }
+                    }
                     connection.Open();
-                    string insertsql = $"INSERT INTO kalaplengetőverseny_pdd.user (`ID`, `Nev`, `pillanatnyihelyezes`, `pont1`, `ido1`, `pont2`, `ido2`,`pont3`, `ido3`,`legjobbpont`, `legjobbido`) VALUES (null,@nev,@loginName,@aktiv,@salt,@hash,@szuldat)";
+                    string insertsql = $"INSERT INTO kalaplengetőverseny_pdd.versenyzok (`ID`, `Nev`, `pillanatnyihelyezes`, `pont1`, `ido1`, `pont2`, `ido2`,`pont3`, `ido3`,`legjobbpont`, `legjobbido`) VALUES (null,@nev,@pillanatnyihelyezes,@pont1,@ido1,@pont2,@ido2,@pont3,@ido3,@legjobbpont,@legjobbido)";
                     MySqlCommand insertCmd = new MySqlCommand(insertsql, connection);
                     insertCmd.Parameters.AddWithValue("@nev", nev);
-                    insertCmd.Parameters.AddWithValue("@loginName", loginName);
-                    insertCmd.Parameters.AddWithValue("@aktiv", aktiv);
-                    insertCmd.Parameters.AddWithValue("@salt", salt);
-                    insertCmd.Parameters.AddWithValue("@hash", hash);
-                    insertCmd.Parameters.AddWithValue("@szuldat", szuldat);
+                    insertCmd.Parameters.AddWithValue("@pillanatnyihelyezes", pillanatnyihelyezes);
+                    insertCmd.Parameters.AddWithValue("@pont1", pont1);
+                    insertCmd.Parameters.AddWithValue("@ido1", ido1);
+                    insertCmd.Parameters.AddWithValue("@pont2", pont2);
+                    insertCmd.Parameters.AddWithValue("@ido2", ido2);
+                    insertCmd.Parameters.AddWithValue("@pont3", pont3);
+                    insertCmd.Parameters.AddWithValue("@ido3", ido3);
+                    insertCmd.Parameters.AddWithValue("@legjobbpont", legjobbpont);
+                    insertCmd.Parameters.AddWithValue("@legjobbido", legjobbido);
                     int sorokszama = insertCmd.ExecuteNonQuery();
                     string valasz = sorokszama > 0 ? "sikeres" : "sikertelen";
                     Console.WriteLine(valasz);
